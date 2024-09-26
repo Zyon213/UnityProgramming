281d08671c356d3e1cf9b1b6e543e8262ed572fb
zyon213 <yg.mms2@gmail.com>
2024-09-26 14:32:47 +0400

    car game

---
+++ First/Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // get the intial postion of the camera
+        offset = transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        // add the initial postion of the camera to the current player position
+        transform.position = offset + player.transform.position;
+    }
+}
---
+++ First/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // private variables
+    private float horizontalInput;
+    private float verticalInput;
+    private float speed = 20.0f;
+    private float turnSpeed = 40.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get Input values
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        // move the car forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        // rotate the car
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+    }
+}
