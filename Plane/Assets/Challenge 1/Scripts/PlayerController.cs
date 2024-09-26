22f3d4daa0793ce6329bb145bb282d24da2f5aab
zyon213 <yg.mms2@gmail.com>
2024-09-26 19:15:38 +0400

    plane game added

---
+++ Plane/Assets/Challenge 1/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float planeSpeed = 10.0f;
+    private float turnSpeed = 1.0f;
+    private float forwardInput;
+    private float horzontalInput;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        forwardInput = Input.GetAxis("Vertical");
+        horzontalInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * planeSpeed);
+       transform.Rotate(Vector3.up * turnSpeed * horzontalInput);
+        transform.Rotate(Vector3.left * turnSpeed * forwardInput);
+    
+    }
+}
